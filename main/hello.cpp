731bcb2b00c41c6f8467567c99bf9d50de64f6a5
bharathithal <bharathithal5@gmail.com>
2023-02-16 11:17:31 +0530

    5th commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,7 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "Suiiii";
-  cout << "Krishna Ronaldo"
+  cout << "Krishna Ronaldo";
   cout << "I have successfully built and run\n";
   return 0;
 }
